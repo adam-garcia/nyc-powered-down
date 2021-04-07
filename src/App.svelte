86bab5847cfb17270f118bad9b69eb563fd6a848
Adam Garcia <hi@adam-garcia.net>
2021-04-07 01:00:45 -0400

    making a little progress, homepage etc

--- src/App.svelte
+++ src/App.svelte
@@ -3,8 +3,17 @@
   import { Router, Route, Link } from "svelte-routing";
   import Home from "./views/Home.svelte";
   import About from "./views/About.svelte";
+	import fetchLatestData from "./data/fetchLatestData.js"
+	import parseCSVData from "./data/parseCSVData.js"
 
   export let url = "";
+
+
+	let nyc311Data;
+	fetchLatestData().then((res) => {
+		nyc311Data = parseCSVData(res.data)
+	});
+
 </script>
 
 <Router url="{url}">
@@ -14,7 +23,7 @@
 	 </nav>
 
 	 <div>
-		 <Route path="/"><Home /></Route>
+		 <Route path="/"><Home {nyc311Data} /></Route>
 		 <Route path="about" component="{About}" />
 	 </div>
  </Router>
---
+++ src/data/parseCSVData.js
@@ -0,0 +1,37 @@
+import * as d3 from 'd3-dsv';
+import dayjs from 'dayjs';
+
+const parseCSVData = (rawData) => {
+  return d3.csvParse(rawData).map((incident, i) => {
+    return {
+      index_number: i,
+      unique_key: incident.unique_key,
+      created_date: dayjs(incident.created_date),
+      closed_date: dayjs(incident.closed_date),
+      agency: incident.agency,
+      agency_name: incident.agency_name,
+      complaint_type: incident.complaint_type,
+      descriptor: incident.descriptor,
+      location_type: incident.location_type,
+      incident_zip: incident.incident_zip,
+      incident_address: incident.incident_address,
+      street_name: incident.street_name,
+      address_type: incident.address_type,
+      city: incident.city,
+      status: incident.status,
+      resolution_description: incident.resolution_description,
+      resolution_action_updated_date: dayjs(incident.resolution_action_updated_date),
+      community_board: incident.community_board,
+      bbl: incident.bbl,
+      borough: incident.borough,
+      x_coordinate_state_plane: incident.x_coordinate_state_plane,
+      y_coordinate_state_plane: incident.y_coordinate_state_plane,
+      open_data_channel_type: incident.open_data_channel_type,
+      park_facility_name: incident.park_facility_name,
+      latitude: +incident.latitude,
+      longitude: +incident.longitude,
+    }
+  })
+}
+
+export default parseCSVData;

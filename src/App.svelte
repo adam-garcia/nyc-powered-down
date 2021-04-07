baad74d4c38f91ad3cddf0e0153dee3fa06245d0
Adam Garcia <hi@adam-garcia.net>
2021-04-06 22:20:52 -0400

    initial commit
    + some routing help from https://medium.com/globant/walking-down-the-svelte-route-67922ae16762

---
+++ src/App.svelte
@@ -0,0 +1,20 @@
+
+<script>
+  import { Router, Route, Link } from "svelte-routing";
+  import Home from "./views/Home.svelte";
+  import About from "./views/About.svelte";
+
+  export let url = "";
+</script>
+
+<Router url="{url}">
+	<nav>
+		 <Link to="/">Home</Link>
+		 <Link to="about">About</Link>
+	 </nav>
+
+	 <div>
+		 <Route path="/"><Home /></Route>
+		 <Route path="about" component="{About}" />
+	 </div>
+ </Router>
